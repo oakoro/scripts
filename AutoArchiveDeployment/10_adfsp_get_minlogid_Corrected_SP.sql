1c551d4177e8a9c2b56e571dd39da2b1c57d2ba2
Oke Akoro <oke.akoro@sscinc.com>
2023-08-30 11:53:06 +0100

    update

--- AutoArchiveDeployment/10_adfsp_get_minlogid_Corrected_SP.sql
+++ AutoArchiveDeployment/10_adfsp_get_minlogid_Corrected_SP.sql
@@ -1,9 +1,17 @@
+/****** Object:  StoredProcedure [BPC].[adfsp_get_minlogidTestOA]    Script Date: 30/08/2023 11:47:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
 -- ==============================================================================
 -- Description: Get minimum logid to copy
--- Usage: [BPC].[adfsp_get_minlogidTestOA] 'BPASessionLog_NonUnicode'
+-- Usage: EXEC [BPC].[adfsp_get_minlogid] 'BPASessionLog_NonUnicode'
 -- ==============================================================================
 
-CREATE PROCEDURE [BPC].[adfsp_get_minlogidTestOA] 
+CREATE PROCEDURE [BPC].[adfsp_get_minlogidTestOA]
 	 @tableName NVARCHAR(255)
 
 
@@ -20,14 +28,14 @@ DECLARE @sqlCommandWM NVARCHAR(1000),
 	@minLogid BIGINT
 
 
-SET @sqlCommandWM = N'SELECT @minlogidWM = logid  FROM bpc.adf_watermark where tableName = ''' + @tableName + ''';'
-SET @ParmDefinition = N'@tableName varchar(255), @minlogidWM BIGINT OUTPUT';
+SET @sqlCommandWM = N'SELECT @minlogidWM = logid  FROM bpc.adf_watermark WHERE tableName = ''' + @tableName + ''';'
+SET @ParmDefinition = N'@tableName NVARCHAR(255), @minlogidWM BIGINT OUTPUT';
 EXEC sp_EXECutesql @sqlCommandWM, @ParmDefinition, @tableName = @tableName, @minlogidWM = @minlogidWMOUT OUTPUT;
 
 
 
-SET @sqlCommandActual = N'SELECT top 1 @minlogidActual = logid from dbo.'+@tableName +' with (nolock) where logid > '+CONVERT(NVARCHAR(20),@minlogidWMOUT)
-SET @ParmDefinition1 = N'@tableName varchar(255), @minlogidActual BIGINT OUTPUT';
+SET @sqlCommandActual = N'SELECT TOP 1 @minlogidActual = logid FROM dbo.'+@tableName +' WITH (nolock) WHERE logid > '+CONVERT(NVARCHAR(20),@minlogidWMOUT) +' ORDER BY logid'
+SET @ParmDefinition1 = N'@tableName NVARCHAR(255), @minlogidActual BIGINT OUTPUT';
 EXEC sp_EXECutesql @sqlCommandActual, @ParmDefinition1, @tableName = @tableName, @minlogidActual = @minlogidActualOUT OUTPUT;
 
 
@@ -37,3 +45,6 @@ SELECT @minlogidActualOUT AS 'minlogid'
 END
 ELSE
 SELECT @minlogidWMOUT AS 'minlogid'
+GO
+
+
