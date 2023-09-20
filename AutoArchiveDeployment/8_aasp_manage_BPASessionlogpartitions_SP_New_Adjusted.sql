9abda05615c379d9691c9e4775c438ec0ca83b45
Oke Akoro <oke.akoro@sscinc.com>
2023-09-20 12:42:38 +0100

    updates

--- AutoArchiveDeployment/8_aasp_manage_BPASessionlogpartitions_SP_New_Adjusted.sql
+++ AutoArchiveDeployment/8_aasp_manage_BPASessionlogpartitions_SP_New_Adjusted.sql
@@ -15,7 +15,7 @@ AS
 IF EXISTS(SELECT 1 FROM sys.indexes i     
 			INNER JOIN sys.partition_schemes ps   
 			ON i.data_space_id = ps.data_space_id
-WHERE OBJECT_NAME(object_id) in (''BPASessionLog_NonUnicode'',''BPASessionLog_Unicode''))
+WHERE OBJECT_NAME(object_id) in (''BPASessionLog_NonUnicode''))
 BEGIN
 -- Create new partition if required - Call [BPC].[aasp_create_New_Sessionlog_partition]  .
 EXECUTE [BPC].[aasp_create_New_Sessionlog_partition] 
@@ -29,7 +29,7 @@ EXECUTE [BPC].[aasp_delete_copied_Sessionlog_partition]
   ,@tablename = ''BPASessionLog_NonUnicode''
   ,@partitionfunction = ''PF_Dynamic_NU''
   ,@partitionsretained = 5;
-  END
+END
   '
 EXECUTE SP_EXECUTESQL @aasp_manage_BPASessionlogpartitions
 END
@@ -41,6 +41,7 @@ DECLARE @alter_aasp_manage_BPASessionlogpartitions NVARCHAR(MAX) = '
 -- Description: Master stored procedure that calls BPASessionlog partition management SPs
 -- Script execution Example: BPC.aasp_manage_BPASessionlogpartitions
 -- Alter statement added to change partition retention from 6 to 5
+-- Script adjusted to check for data partitioning before run
 -- =======================================================================================
 
 ALTER PROCEDURE [BPC].[aasp_manage_BPASessionlogpartitions]
@@ -49,7 +50,7 @@ AS
 IF EXISTS(SELECT 1 FROM sys.indexes i     
 			INNER JOIN sys.partition_schemes ps   
 			ON i.data_space_id = ps.data_space_id
-WHERE OBJECT_NAME(object_id) in (''BPASessionLog_NonUnicode'',''BPASessionLog_Unicode''))
+WHERE OBJECT_NAME(object_id) in (''BPASessionLog_NonUnicode''))
 BEGIN
 -- Create new partition if required - Call [BPC].[aasp_create_New_Sessionlog_partition]  .
 EXECUTE [BPC].[aasp_create_New_Sessionlog_partition] 
@@ -63,7 +64,7 @@ EXECUTE [BPC].[aasp_delete_copied_Sessionlog_partition]
   ,@tablename = ''BPASessionLog_NonUnicode''
   ,@partitionfunction = ''PF_Dynamic_NU''
   ,@partitionsretained = 5;
-  END'
+END'
 
 EXECUTE SP_EXECUTESQL @alter_aasp_manage_BPASessionlogpartitions
 END
--- AutoArchiveDeployment/Old/8_aasp_manage_BPASessionlogpartitions_SP.sql
+++ AutoArchiveDeployment/Old/8_aasp_manage_BPASessionlogpartitions_SP.sql
@@ -12,15 +12,10 @@ DECLARE @aasp_manage_BPASessionlogpartitions NVARCHAR(MAX) = '
 CREATE PROCEDURE [BPC].[aasp_manage_BPASessionlogpartitions]
 
 AS
-IF EXISTS(SELECT 1 FROM sys.indexes i     
-			INNER JOIN sys.partition_schemes ps   
-			ON i.data_space_id = ps.data_space_id
-WHERE OBJECT_NAME(object_id) in (''BPASessionLog_NonUnicode''))
-BEGIN
 -- Create new partition if required - Call [BPC].[aasp_create_New_Sessionlog_partition]  .
 EXECUTE [BPC].[aasp_create_New_Sessionlog_partition] 
    @tablename = ''[dbo].[BPASessionLog_NonUnicode]''
-  ,@partitionfunction = ''PF_Dynamic_NU'';
+  ,@partitionfunction = ''PF_Dynamic_NU''
 
 
 -- Drop copied partition - Call [BPC].[aasp_delete_copied_Sessionlog_partition] 
@@ -28,8 +23,7 @@ EXECUTE [BPC].[aasp_delete_copied_Sessionlog_partition]
    @tableschema = ''dbo''
   ,@tablename = ''BPASessionLog_NonUnicode''
   ,@partitionfunction = ''PF_Dynamic_NU''
-  ,@partitionsretained = 5;
-END
+  ,@partitionsretained = 5
   '
 EXECUTE SP_EXECUTESQL @aasp_manage_BPASessionlogpartitions
 END
@@ -41,21 +35,15 @@ DECLARE @alter_aasp_manage_BPASessionlogpartitions NVARCHAR(MAX) = '
 -- Description: Master stored procedure that calls BPASessionlog partition management SPs
 -- Script execution Example: BPC.aasp_manage_BPASessionlogpartitions
 -- Alter statement added to change partition retention from 6 to 5
--- Script adjusted to check for data partitioning before run
 -- =======================================================================================
 
 ALTER PROCEDURE [BPC].[aasp_manage_BPASessionlogpartitions]
 
 AS
-IF EXISTS(SELECT 1 FROM sys.indexes i     
-			INNER JOIN sys.partition_schemes ps   
-			ON i.data_space_id = ps.data_space_id
-WHERE OBJECT_NAME(object_id) in (''BPASessionLog_NonUnicode''))
-BEGIN
 -- Create new partition if required - Call [BPC].[aasp_create_New_Sessionlog_partition]  .
 EXECUTE [BPC].[aasp_create_New_Sessionlog_partition] 
    @tablename = ''[dbo].[BPASessionLog_NonUnicode]''
-  ,@partitionfunction = ''PF_Dynamic_NU'';
+  ,@partitionfunction = ''PF_Dynamic_NU''
 
 
 -- Drop copied partition - Call [BPC].[aasp_delete_copied_Sessionlog_partition] 
@@ -63,10 +51,7 @@ EXECUTE [BPC].[aasp_delete_copied_Sessionlog_partition]
    @tableschema = ''dbo''
   ,@tablename = ''BPASessionLog_NonUnicode''
   ,@partitionfunction = ''PF_Dynamic_NU''
-  ,@partitionsretained = 5;
-END'
+  ,@partitionsretained = 5'
 
 EXECUTE SP_EXECUTESQL @alter_aasp_manage_BPASessionlogpartitions
 END
-
-
