64db676ed511aa637aa41778e3fc7ff3ad38c9f0
Oke Akoro <oke.akoro@sscinc.com>
2024-11-22 16:14:55 +0000

    update

--- AutoArchiveDeployment/Changes/JiraPE-2317/SQL/cleanup.sql
+++ AutoArchiveDeployment/Changes/JiraPE-2317/SQL/cleanup.sql
@@ -7,3 +7,18 @@ select * from BPC.adf_watermark
 select * from sys.procedures order by create_date desc
 
 --drop proc bpc.aasp_getCopyDatesDelta3
+
+
+
+
+--;with bpasession_upd
+--as
+--(
+--select top 10* from dbo.BPASession
+--where enddatetime is null
+--)
+--update bpasession_upd
+--set enddatetime = GETDATE()
+
+--BPAAuditEvents	eventdatetime	2021-07-16 21:21:25.780	NULL
+--BPASession	enddatetime	2024-11-20 16:36:12.017	NULL
--- Utilities/deadlock1.sql
+++ Utilities/deadlock1.sql
@@ -1,24 +1,20 @@
---SELECT 
---	#OfDeadlockEvents = format(sum(event_count),'#,#')
---FROM
---	sys.event_log 
---WHERE 
---	event_type = 'deadlock'
-
-	select * FROM
-	sys.event_log 
-WHERE event_type = 'deadlock' order by 3 desc
-
-
 WITH CTE AS (
 SELECT 
     CAST(event_data AS XML)  AS [target_data_XML]
 FROM 
     sys.fn_xe_telemetry_blob_target_read_file('dl', null, null, null)
 )
+,
+CTE2
+AS
+(
 SELECT 
     target_data_XML.value('(/event/@timestamp)[1]', 'DateTime2') AS Timestamp,
     target_data_XML.query('/event/data[@name=''xml_report'']/value/deadlock') AS deadlock_xml,
     target_data_XML.query('/event/data[@name=''database_name'']/value').value('(/value)[1]', 'nvarchar(100)') AS db_name
 FROM CTE
-ORDER BY Timestamp DESC
+)
+SELECT CAST(Timestamp AS DATE)'DAY', COUNT(*)
+FROM CTE2
+GROUP BY CAST(Timestamp AS DATE)
+ORDER BY [DAY]
