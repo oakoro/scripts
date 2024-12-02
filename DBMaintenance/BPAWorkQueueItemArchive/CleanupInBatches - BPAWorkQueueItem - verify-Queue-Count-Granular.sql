698d923bc3fc0b8565be5e69493bb9668d27c95f
Oke Akoro <oakoro@ssnc-corp.global>
2024-12-02 17:57:48 +0000

    updates

---
+++ DBMaintenance/BPAWorkQueueItemArchive/CleanupInBatches - BPAWorkQueueItem - verify-Queue-Count-Granular.sql
@@ -0,0 +1,77 @@
+
+-- Declare variables
+DECLARE @DaysToKeep INT;
+DECLARE @Threshold DATETIME;
+DECLARE @ERROR_SEVERITY INT;
+DECLARE @ERROR_STATE INT;
+DECLARE @ERROR_NUMBER INT;
+DECLARE @ERROR_LINE INT;
+DECLARE @ERROR_MESSAGE NVARCHAR(4000);
+DECLARE @Queuename NVARCHAR(200) = 'Activation Checklist'
+DECLARE @QueuesToInclude TABLE
+(
+    QueueID UNIQUEIDENTIFIER NULL,
+    QueueName NVARCHAR(255) NOT NULL
+);
+
+-- Set variables, modify the @DaysToKeep to reflect the required retention period
+SET @DaysToKeep = 365;
+SET @Threshold = CONVERT(DATE, GETDATE() - @DaysToKeep);
+
+-- Create maintenance table to hold the WorkQueueItem(s) we want to delete
+IF OBJECT_ID(N'DeletedWorkQueueItems_Maintenance', N'U') IS NULL
+BEGIN
+    CREATE TABLE [DeletedWorkQueueItems_Maintenance]
+    (
+        WorkQueueItemID UNIQUEIDENTIFIER NOT NULL
+    );
+END;
+
+-- Insert QueueNames, update the VALUES below to reflext the name of the Queues you want to target
+INSERT INTO @QueuesToInclude (QueueName)
+VALUES (@Queuename);
+
+-- Update QueueIDs
+UPDATE @QueuesToInclude
+SET QI.QueueID = Q.id
+FROM BPAWorkQueue AS Q WITH (NOLOCK) 
+INNER JOIN @QueuesToInclude AS QI ON QI.QueueName = Q.[name];
+
+-- If any of the queue names do not exits stop the script and print and error
+IF EXISTS (SELECT 1 FROM @QueuesToInclude WHERE QueueID IS NULL)
+BEGIN
+    PRINT 'One or more of the queue names provided does not exist see the results tab for these queues, update the names and re-run the query. No data has been modified, the script has been terminated';
+    SELECT 'This Queue Does Not Exist',
+           QueueName
+    FROM @QueuesToInclude
+    WHERE QueueID IS NULL;
+    RETURN;
+END;
+
+-- Get WorkQueueItem(s) we want to delete
+
+
+		INSERT INTO DeletedWorkQueueItems_Maintenance
+		(
+			WorkQueueItemID
+		)
+		SELECT I.id
+		FROM BPAWorkQueueItem AS I
+		INNER JOIN @QueuesToInclude AS QI
+			ON QI.QueueID = I.queueid
+		LEFT JOIN BPAWorkQueueItem AS inext
+			ON I.id = inext.id
+			   AND inext.attempt = I.attempt + 1
+		LEFT JOIN DeletedWorkQueueItems_Maintenance AS WM
+			ON WM.WorkQueueItemID = I.id
+		WHERE inext.id IS NULL
+			  AND WM.WorkQueueItemID IS NULL
+			  --AND I.finished < @Threshold
+			  AND datepart(year,convert(date,I.finished)) = '2024'
+			  AND datepart(month,convert(date,I.finished)) = '5'
+			  --AND datepart(week,convert(date,I.finished)) = '14'
+			  AND QI.QueueID IS NOT NULL;
+
+	SELECT COUNT(*) FROM DeletedWorkQueueItems_Maintenance
+
+
--- DBMaintenance/BPAWorkQueueItemArchive/CleanupWorkQueueItemInBatches/CleanupInBatches - BPAWorkQueueItem - SpecificQueues-CO2302-CB8 - Connells Group.sql
+++ DBMaintenance/BPAWorkQueueItemArchive/CleanupWorkQueueItemInBatches/CleanupInBatches - BPAWorkQueueItem - SpecificQueues-CO2302-CB8 - Connells Group.sql
@@ -1,7 +1,7 @@
 /*
-	Name:			CleanupInBatches - BPAWorkQueueItem.sql
+	Name:			CleanupInBatches - BPAWorkQueueItem - SpecificQueues.sql
 	Author:			Blue Prism
-	Date:			20220330
+	Date:			20221019
 	Description:	This script is to be used to clean up the BPAWorkQueueItem, BPATag and BPAWorkQueueLog tables in scenarios where the standard maintenance 
 					scripts do not complete in a timely fashion. By default it will retain the last 7 days of data in the BPAWorkQueueItem table and BPATag records 
 					that are not associated with a work queue item.
@@ -9,7 +9,14 @@
 	BP Version(s):	This script has been tested against Version 6.4+ of Blue Prism
 	Usage:			The "Blue Prism Database Name Here" needs replacing with the name of your Blue Prism database
 					The @DaysToKeep variable is to be updated where a retention period other than 7 days is required
-	
+					The list of Queue Names to include needs to be modified this can be found on line 95 as below.
+
+					VALUES ('Queue2'),('Queue3'),('Test1');
+
+					If you wanted to only clean up queue items from queues called SalesOrderProcessing and PurchaseOrderProcessing then then line 95 would be updated as below.
+
+					VALUES ('SalesOrderProcessing'),('PurchaseOrderProcessing');
+					
 	This script can potentially delete tens of gigabytes of data from the BPAWorkQueueItem table
 	and will impact the size of your transaction log file as such it is recommended that; 
 		- The person running the script should have a good understanding of how transactions and transaction log backups impact the transaction log of a database
@@ -40,7 +47,7 @@
 	*** Whilst this script is re-runnable it does make use of transactions, if the script is stopped when manually being executed in management studio there is a possibility
 	*** that there may still be an open transaction, this can safely be rolled back and only the last batch will be undone. This can be achieved by either
 	***	closing the query window and clicking "No" on the resulting "There are uncommitted transactions. Do you with to commit these transactions?" prompt or by
-	*** highlighting only the words "ROLLBACK TRANSACTION;" in the script and executing this single line (line number 109 for example)	
+	*** highlighting only the words "ROLLBACK TRANSACTION;" in the script and executing this single line (line number 145 for example)	
 
 	*** The script is to be tested in lower route to live environments (Development / Test / UAT / SIT) and the results verified before being run against a Production environment
 
@@ -53,8 +60,7 @@
 */
 
 -- Set database context
---USE [Blue Prism Database Name Here];
---GO
+
 
 -- Declare variables
 DECLARE @DaysToKeep INT;
@@ -64,10 +70,15 @@ DECLARE @ERROR_STATE INT;
 DECLARE @ERROR_NUMBER INT;
 DECLARE @ERROR_LINE INT;
 DECLARE @ERROR_MESSAGE NVARCHAR(4000);
-
+DECLARE @Queuename NVARCHAR(200) = 'Activation Checklist'
+DECLARE @QueuesToInclude TABLE
+(
+    QueueID UNIQUEIDENTIFIER NULL,
+    QueueName NVARCHAR(255) NOT NULL
+);
 
 -- Set variables, modify the @DaysToKeep to reflect the required retention period
-SET @DaysToKeep = 14;
+SET @DaysToKeep = 365;
 SET @Threshold = CONVERT(DATE, GETDATE() - @DaysToKeep);
 
 -- Create maintenance table to hold the WorkQueueItem(s) we want to delete
@@ -79,24 +90,51 @@ BEGIN
     );
 END;
 
+-- Insert QueueNames, update the VALUES below to reflext the name of the Queues you want to target
+INSERT INTO @QueuesToInclude (QueueName)
+VALUES (@Queuename);
+
+-- Update QueueIDs
+UPDATE @QueuesToInclude
+SET QI.QueueID = Q.id
+FROM BPAWorkQueue AS Q WITH (NOLOCK) 
+INNER JOIN @QueuesToInclude AS QI ON QI.QueueName = Q.[name];
+
+-- If any of the queue names do not exits stop the script and print and error
+IF EXISTS (SELECT 1 FROM @QueuesToInclude WHERE QueueID IS NULL)
+BEGIN
+    PRINT 'One or more of the queue names provided does not exist see the results tab for these queues, update the names and re-run the query. No data has been modified, the script has been terminated';
+    SELECT 'This Queue Does Not Exist',
+           QueueName
+    FROM @QueuesToInclude
+    WHERE QueueID IS NULL;
+    RETURN;
+END;
+
 -- Get WorkQueueItem(s) we want to delete
 BEGIN TRY
     BEGIN TRANSACTION;
 
-    INSERT INTO DeletedWorkQueueItems_Maintenance
-    (
-        WorkQueueItemID
-    )
-    SELECT I.id
-    FROM BPAWorkQueueItem AS I with (nolock)
-    LEFT JOIN BPAWorkQueueItem AS inext with (nolock)
-        ON I.id = INEXT.id
-           AND INEXT.attempt = I.attempt + 1
-    LEFT JOIN DeletedWorkQueueItems_Maintenance AS WM
-        ON WM.WorkQueueItemID = I.id
-    WHERE INEXT.id IS NULL
-          AND WM.WorkQueueItemID IS NULL
-          AND I.finished < @threshold;
+		INSERT INTO DeletedWorkQueueItems_Maintenance
+		(
+			WorkQueueItemID
+		)
+		SELECT I.id
+		FROM BPAWorkQueueItem AS I
+		INNER JOIN @QueuesToInclude AS QI
+			ON QI.QueueID = I.queueid
+		LEFT JOIN BPAWorkQueueItem AS inext
+			ON I.id = inext.id
+			   AND inext.attempt = I.attempt + 1
+		LEFT JOIN DeletedWorkQueueItems_Maintenance AS WM
+			ON WM.WorkQueueItemID = I.id
+		WHERE inext.id IS NULL
+			  AND WM.WorkQueueItemID IS NULL
+			  --AND I.finished < @Threshold
+			  AND datepart(year,convert(date,I.finished)) = '2024'
+			  AND datepart(month,convert(date,I.finished)) = '6'
+			  --AND datepart(week,convert(date,I.finished)) = '14'
+			  AND QI.QueueID IS NOT NULL;
 
     COMMIT TRANSACTION;
 END TRY
@@ -118,7 +156,7 @@ BEGIN CATCH
     RAISERROR('Msg %d, Line %d, : %s', @ERROR_SEVERITY, @ERROR_STATE, @ERROR_NUMBER, @ERROR_LINE, @ERROR_MESSAGE);
 END CATCH;
 
--- Delete BPAWorkQueueItem1 records in batches of 25,000
+-- Delete BPAWorkQueueItem records in batches of 25,000
 WHILE (2 > 1)
 BEGIN TRY
     BEGIN TRANSACTION;
@@ -138,7 +176,7 @@ BEGIN TRY
     -- Commit transaction
     COMMIT TRANSACTION;
 
-    -- 1 second delay to allow other operations to access BPAWorkQueueItem1
+    -- 1 second delay to allow other operations to access BPAWorkQueueItem
     WAITFOR DELAY '00:00:01';
 END TRY
 
